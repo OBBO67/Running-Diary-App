9a99598def23934d9b33fe97ad61ee82b18e5c83
Owen <owenbrady1994@gmail.com>
2018-12-08 13:40:05 +0000

    Add landing page route. Add ejs. Add body-parser.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Initial test");
-});
+// require routes
+const indexRoutes = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
+
+app.use("/", indexRoutes);
 
 app.listen("1000", () => {
   console.log("Server running on port 1000");
