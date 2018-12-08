09a0bd9e689c76b528d71974d6f2852e29732f70
Owen <owenbrady1994@gmail.com>
2018-12-08 15:38:19 +0000

    Style signup and login buttons. Add login route and form. Add signup route and form.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
   res.render("landing");
 });
 
+// Login Page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Register Page
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
 module.exports = router;
